be3a6a611cb70d89c6310dcb126a59397e8e0ff6
LemonTeatw1 <laol04916@gmail.com>
2025-01-15 22:27:11 +0800

    update

--- port_scanner.py
+++ port_scanner.py
@@ -6,13 +6,15 @@ from concurrent.futures import ThreadPoolExecutor
 import threading
 
 print_lock = threading.Lock()
+
 scanned_ports = set()
+
 total_ports = 65535
 
 def scan_port(target_ip, port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            socket.setdefaulttimeout(0.1)
+            s.settimeout(0.1)
             result = s.connect_ex((target_ip, port))
             with print_lock:
                 scanned_ports.add(port)
@@ -20,21 +22,22 @@ def scan_port(target_ip, port):
                     print(f"\033[92mPort {port} is open\033[0m")
                 else:
                     progress = len(scanned_ports) / total_ports * 100
-                    print(f"Progress: {progress:.1f} % ({len(scanned_ports)}/{total_ports})", end='\r')
-    except:
+                    print(f"Progress: {progress:.1f}%", end='\r')
+    except Exception as e:
         pass
 
 def main():
-    ascii_banner = pyfiglet.figlet_format("OWO PORT SCANNER")
+    
+    ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
     print(ascii_banner)
-    
+
     target = input("Enter the target to scan (e.g., IP address or hostname): ")
-    
+
     try:
         target_ip = socket.gethostbyname(target)
     except socket.gaierror:
         print("\n[!] Hostname Could Not Be Resolved!")
-        sys.exit()
+        sys.exit(1)
 
     print("-" * 50)
     print(f"Scanning Target: {target_ip}")
@@ -42,17 +45,17 @@ def main():
     print("-" * 50)
 
     try:
-        with ThreadPoolExecutor(max_workers=1000) as executor:
-            for port in range(1, 65536):
+        with ThreadPoolExecutor(max_workers=500) as executor:
+            for port in range(1, total_ports + 1):
                 executor.submit(scan_port, target_ip, port)
-        print("\n\nScan completed !")
-                
+
+        print("\n\nScan completed successfully!")
     except KeyboardInterrupt:
         print("\n[!] Scan terminated by user")
-        sys.exit()
+        sys.exit(0)
     except socket.error:
         print("\n[!] Server not responding!")
-        sys.exit()
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
